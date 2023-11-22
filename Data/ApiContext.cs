66ebf5623c05d570b98a85974f83ee6993eedb16
Shivam Upadhyay <shivamupadhyay@Macbook-Shivam.local>
2023-11-22 10:45:07 +0530

    Fix formatting

--- Data/ApiContext.cs
+++ Data/ApiContext.cs
@@ -3,14 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using practices.Models;
 
-namespace practices.Data {
-    public class ApiContext: IdentityDbContext<ApplicationUser> {
+namespace practices.Data
+{
+    public class ApiContext : IdentityDbContext<ApplicationUser>
+    {
 
         public ApiContext(DbContextOptions<ApiContext> options)
             : base(options)
-            {}
+        { }
 
-        public DbSet<Blog> Blogs { get; set; }        
+        public DbSet<Blog> Blogs { get; set; }
     }
 }
-    
+
